905183ef1a3fc9b53672d21612319e8f0175c90e
rohitmalviya03 <rmrohit439@gmail.com>
2023-04-11 11:54:56 +0530

    first commit

---
+++ src/components/UserManual.js
@@ -0,0 +1,126 @@
+import React from 'react';
+
+export default function UserManual() {
+  return (
+    <div class="container px-5 my-5">
+    <div class="text-center mb-2">
+                  <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">User Guide</span></h1>
+                
+     </div>
+    <div class="row gx-5 justify-content-center">
+        <div class="col-lg-11 col-xl-9 col-xxl-8">
+            
+            <section>
+                <div class="d-flex align-items-center justify-content-between mb-4">
+                     
+                </div>
+                 <div class="card shadow border-0 rounded-4 mb-5">
+                    <div class="card-body p-5">
+                        <div class="row align-items-center gx-5">
+                         
+                            <div class="col-lg-12">
+                            <h5 class="text-primary fw-bolder mb-0">Creating an Account</h5>
+                    <div>
+                                To get started, you need to create an account on the Interview Recording Platform. Follow these steps:
+<ol><li>
+Go to the platform's website and click on the "Sign Up" button.    
+    </li>
+    <li>Enter your email address, a strong password, and any other required information.</li>
+    <li>Click on the "Create Account" button to submit your information.</li>
+    </ol>
+
+
+</div>
+
+
+</div>
+                        </div>
+
+                        <div class="row align-items-center gx-5">
+                         
+                            <div class="col-lg-12">
+                            <h5 class="text-primary fw-bolder mb-0">Logging In</h5>
+                    <div>
+                    Once you have created your account, you can log in to the platform. Follow these steps:
+<br></br>
+<ol><li>
+Go to the platform's website and click on the "Log In" button.  
+    </li>
+    <li>Enter your email address and password.</li>
+    <li>Click on the "Log In" button to access your dashboard.  </li>
+    </ol>
+
+
+</div>
+
+
+</div>
+                        </div>
+
+
+                        <div class="row align-items-center gx-5">
+                         
+                         <div class="col-lg-12">
+                         <h5 class="text-primary fw-bolder mb-0">Selecting a Language</h5>
+                 <div>
+                 After you log in, you will be taken to your dashboard. From here, you can select the language for which you want to give an interview. Follow these steps:
+<br></br>
+<ol><li>
+Click on the "Select Language" button on your dashboard.  
+ </li>
+ <li>Choose the language for which you want to give an interview from the drop-down menu.</li>
+ <li>Click on the "Submit" button to confirm your selection. </li>
+ </ol>
+
+
+</div>
+
+
+</div>
+                     </div>    
+
+                     <div class="row align-items-center gx-5">
+                         
+                         <div class="col-lg-12">
+                         <h5 class="text-primary fw-bolder mb-0">Starting an Interview</h5>
+                 <div>
+                 Once you have selected a language, you can start an interview. Follow these steps:
+<br></br>
+<ol><li>
+Click on the "Start Interview" button on your dashboard.  
+ </li>
+ <li>You will be redirected to the interview window.</li>
+ <li>Click on the "Record" button to start recording your interview.</li>
+ <li>If you want to listen to the audio of the questions, click on the "Play" button.</li>
+ </ol>
+
+
+</div>
+
+
+</div>
+                     </div> 
+
+                     <div class="row align-items-center gx-5">
+                         
+                         <div class="col-lg-12">
+                         <h5 class="text-primary fw-bolder mb-0">Conclusion</h5>
+                 <div>
+                 That's it! You now know how to use the Interview Recording Platform to create an account, log in, select a language, and start an interview. If you have any questions or encounter any issues, please contact our support team for assistance. 
+ <br></br>
+
+
+
+</div>
+
+
+</div>
+                     </div>   
+                    </div>
+                </div>
+               
+                
+            </section>
+            </div></div></div>
+  );
+}
